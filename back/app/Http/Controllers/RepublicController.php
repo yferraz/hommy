7709cffafb4dc3938a43d11ed93cd8f9ee127cb6
yferraz <yurigabrielsf@gmail.com>
2020-07-29 19:33:11 -0300

    Exercicio_ModelsII_pt1

--- back/app/Http/Controllers/RepublicController.php
+++ back/app/Http/Controllers/RepublicController.php
@@ -11,15 +11,7 @@ class RepublicController extends Controller
 {
     public function createRepublic(RepublicRequest $request){
         $republic = new Republic;
-        $republic->name = $request->name;
-        $republic->zipCode = $request->zipCode;
-        $republic->city = $request->city;
-        $republic->street = $request->street;
-        $republic->number = $request->number;
-        $republic->totalBathroom = $request->totalBathroom;
-        $republic->haveBackyard = $request->haveBackyard;
-        $republic->acceptPets = $request->acceptPets;
-        $republic->save();
+        $republic -> createRepublic($request);
         return response()->json($republic);
     }
 
@@ -33,33 +25,9 @@ class RepublicController extends Controller
         return response()->json([$republic]);
     }
 
-    public function updateRepublic(Request $request, $id){
+    public function updateRepublic(RepublicRequest $request, $id){
         $republic = Republic::findOrFail($id);
-        if($request->name){
-            $republic->name = $request->name;
-        }
-        if($request->zipCode){
-            $republic->zipCode = $request->zipCode;
-        }
-        if($request->city){
-            $republic->city = $request->city;
-        }
-        if($request->street){
-            $republic->street = $request->street;
-        }
-        if($request->number){
-            $republic->number = $request->number;
-        }
-        if($request->totalBathroom){
-            $republic->totalBathroom = $request->totalBathroom;
-        }
-        if($request->haveBackyard){
-            $republic->haveBackyard = $request->haveBackyard;
-        }
-        if($request->acceptPets){
-            $republic->acceptPets = $request->acceptPets;
-        }
-        $republic->save();
+        $republic -> updateRepublic($request);
         return response()->json($republic);
     }
 
--- back/app/Republic.php
+++ back/app/Republic.php
@@ -3,16 +3,61 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
 use App\User;
 use App\Bedroom;
+use App\Http\Requests\RepublicRequest;
 
 class Republic extends Model
 {
-    public function users(){
-      return $this->belongsTo('App\User');
+  public function createRepublic(RepublicRequest $request)
+  {
+    $this->name = $request->name;
+    $this->zipCode = $request->zipCode;
+    $this->city = $request->city;
+    $this->street = $request->street;
+    $this->number = $request->number;
+    $this->totalBathroom = $request->totalBathroom;
+    $this->haveBackyard = $request->haveBackyard;
+    $this->acceptPets = $request->acceptPets;
+    $this->save();
+  }
+
+  public function updateRepublic(RepublicRequest $request){
+    if($request->name){
+        $this->name = $request->name;
     }
-    
-    public function bedrooms(){
-      return $this->hasMany('App\Bedroom');
+    if($request->zipCode){
+        $this->zipCode = $request->zipCode;
     }
+    if($request->city){
+        $this->city = $request->city;
+    }
+    if($request->street){
+        $this->street = $request->street;
+    }
+    if($request->number){
+        $this->number = $request->number;
+    }
+    if($request->totalBathroom){
+        $this->totalBathroom = $request->totalBathroom;
+    }
+    if($request->haveBackyard){
+        $this->haveBackyard = $request->haveBackyard;
+    }
+    if($request->acceptPets){
+        $this->acceptPets = $request->acceptPets;
+    }
+    $this->save();
+}
+
+  public function users()
+  {
+    return $this->belongsTo('App\User');
+  }
+
+  public function bedrooms()
+  {
+    return $this->hasMany('App\Bedroom');
+  }
 }
