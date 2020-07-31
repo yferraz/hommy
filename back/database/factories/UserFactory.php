577a0113fb509e72efb2c216d11a6c9d86f10c36
yferraz <yurigabrielsf@gmail.com>
2020-07-31 19:22:36 -0300

    Exercicio_Passport_1

--- back/database/factories/UserFactory.php
+++ back/database/factories/UserFactory.php
@@ -23,6 +23,8 @@ $factory->define(User::class, function (Faker $faker) {
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'cpf' => $faker->cpf,
+        'dateOfBirth' => $faker->date($format = 'd-m-Y', $max = 'now');
         'remember_token' => Str::random(10),
     ];
 });
